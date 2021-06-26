b3c7ec49cdf91bcc552ed903e8b779ee3e08c834
Emre YILMAZ <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-06-26 10:54:54 +0300

    asdasdasdas

---
+++ app/routes/history.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const axios = require("axios");
+const querystring = require("querystring");
+
+//example history
+// https://live-rates.com/historical/series?base=ETH&start=2021-05-27&end=2021-06-26&symbols=USD&key=cea9fac77f
+
+const app = express.Router();
+
+//**************Route Level 1
+
+const ROOT_URL = "https://live-rates.com/";
+const KEY = "cea9fac77f";
+// Login
+app.post("/", async (req, res) => {
+  let { base, start, end, symbols } = req.body;
+
+  let url = ROOT_URL + "historical/series?";
+
+  url += querystring.stringify({
+    base: base.trim(),
+    start: start.trim(),
+    end: end.trim(),
+    symbols: symbols.trim(),
+    key: KEY,
+  });
+
+  axios
+    .get(url)
+    .then(({ data }) => {
+      return res.json({
+        data,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
+});
+
+module.exports = app;
--- app/server.js
+++ app/server.js
@@ -3,10 +3,29 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
+//routes
+const history = require('./routes/history')
+
+const router = express.Router();
 // CORS PROBLEM FIXED
 app.use(cors())
 // Settings
 const PORT = process.env.PORT || 4000;
+
+
+router.get("/", (req, res) => res.json({ KITE: "ACTIVE" }));
+/**********SERVICES********/
+router.use("/api/history", history);
+
+/**************************/
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//add routes
+app.use(router);
+
 //socket fro frontend and each client
 const server = app.listen(PORT,() => log(`Live Rates Data Server started on port 4000`));
 const socket = require('socket.io');
@@ -17,9 +36,7 @@ const client = require('socket.io-client');
 const socketClient = client('https://wss.live-rates.com/')
 
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 
 //prepare live rate
 const  key = 'cea9fac77f' 
