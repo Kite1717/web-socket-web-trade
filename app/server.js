eeb38db248e99926466019b2470253c7a9481d49
Emre YILMAZ <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-06-26 09:27:13 +0300

    adsfasdas

--- app/server.js
+++ app/server.js
@@ -1,15 +1,8 @@
 const log = console.log;
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
-const cors = require('cors')
 
 app.use(cors())
 // Settings
