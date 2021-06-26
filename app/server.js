0f90a03669516e26527c485016d0bd48940c3f24
Emre YILMAZ <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-06-26 09:12:26 +0300

    adsfasdas

--- app/server.js
+++ app/server.js
@@ -2,6 +2,14 @@ const log = console.log;
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 //socket fro frontend and each client
 const server = app.listen('4000',() => log(`Live Rates Data Server started on port 4000`));
 const socket = require('socket.io');
