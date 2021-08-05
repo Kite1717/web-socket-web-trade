dfed50fd828c3e76f100ff923c3fe55b65b9a364
Kıte <48056832+Kite1717@users.noreply.github.com>
2021-08-05 18:36:03 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -46,7 +46,7 @@ socket.on("connect", function () {
   console.log("disconnecting and reconnecting can take upto a minute");
   console.log(".......");
 
-  socket.emit("login", { userKey: "sioehJ71_sMAGqvELnPSQ" });
+  socket.emit("login", { userKey: "siolIXBmePo_0Xjm1eJHw" });
 });
 
 socket.on("disconnect", function (msg) {
